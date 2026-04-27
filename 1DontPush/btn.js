f0980801db5c9759174224cbf8f0ce03a30a57e4
hassy-5568 <hassy.tsu.zzz@gmail.com>
2026-04-27 19:50:02 +0900

    修正

--- 1DontPush/btn.js
+++ 1DontPush/btn.js
@@ -1,20 +1,22 @@
 const textElement = document.getElementById('text');
 
 const btnElement = document.getElementById('btn');
+// javascriptだと型ないから宣言しないで行けるのか。
 
 let count = 0;
 
 // 昔の度胸試しボタンとか作れそう。
+// ifにするとうまくいかなかった。
 btnElement.addEventListener('click', () => {
   if(count == 0){
     textElement.textContent = "ウイルスが混入しました";
     count++;
   }
-  if(count == 1){
+  else if(count == 1){
     textElement.textContent = "このパソコンのデータを全て消去します";
     count++;
   }
-  if(count >= 2){
+  else if(count >= 2){
     textElement.textContent = "なーんちゃって";
     count=0;
   }
